d47c2749674fefd028be37706c8c291dcbf2b434
Sam Rich <samrichca@users.noreply.github.com>
2018-10-18 21:01:08 -0400

    add redux and tailwind css (#2)
    remove package-lock.json
    
    install packages

--- src/api/index.js
+++ src/api/index.js
@@ -1,16 +1,13 @@
 // base API URL
-const API_BASE_URL =
-  process.env.NODE_ENV === "development"
-    ? ""
-    : "http://woke-company-backend.bridgeschoolapp.io";
+const API_BASE_URL = process.env.NODE_ENV === 'production' ? '' : 'http://woke-company-backend.bridgeschoolapp.io';
 
 // example API method
 export const checkApiServer = () =>
-  fetch(`${API_BASE_URL}/health`)
-    .then(res => res.json())
-    .then(response => {
-      console.log(response);
-    })
-    .catch(err => {
-      console.log(err);
-    });
+	fetch(`${API_BASE_URL}/health`)
+		.then((res) => res.json())
+		.then((response) => {
+			console.log(response);
+		})
+		.catch((err) => {
+			console.log(err);
+		});
