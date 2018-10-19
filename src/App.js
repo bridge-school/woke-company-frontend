29406750c87845965750e97fcad19642494afeb7
Sam Rich <samrichca@users.noreply.github.com>
2018-10-18 20:47:20 -0400

    add redux and tailwind css (#1)
    remove package-lock.json

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import { checkApiServer } from "./api";
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { checkApiServer } from './api';
 
 class App extends Component {
-  componentDidMount() {
-    // checking that we're connected to the backend
-    checkApiServer();
-  }
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+	componentDidMount() {
+		// checking that we're connected to the backend
+		checkApiServer();
+	}
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
+					<h1 className="App-title">Welcome to React</h1>
+				</header>
+				<p className="App-intro">
+					To get started, edit <code>src/App.js</code> and save to reload.
+				</p>
+			</div>
+		);
+	}
 }
 
 export default App;
